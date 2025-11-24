c035a028e3248819961e2b2146a9ba3b44af90f3
Hao079231 <haotrinh2003.com@gmail.com>
2025-11-24 08:01:04 +0700

    login 2 verification use MFA and google authenticator

--- source/user-base-auth/src/main/java/com/base/auth/controller/UserController.java
+++ source/user-base-auth/src/main/java/com/base/auth/controller/UserController.java
@@ -6,9 +6,13 @@ import com.base.auth.dto.ErrorCode;
 import com.base.auth.dto.ResponseListDto;
 import com.base.auth.dto.user.UserAutoCompleteDto;
 import com.base.auth.dto.user.UserDto;
+import com.base.auth.exception.BadRequestException;
+import com.base.auth.exception.NotFoundException;
 import com.base.auth.form.user.SignUpUserForm;
 import com.base.auth.form.user.LoginForm;
 import com.base.auth.form.user.UpdateUserForm;
+import com.base.auth.form.user.UserIdForm;
+import com.base.auth.form.user.VerifyOtpLoginForm;
 import com.base.auth.mapper.AccountMapper;
 import com.base.auth.mapper.UserMapper;
 import com.base.auth.model.Account;
@@ -16,6 +20,7 @@ import com.base.auth.model.Group;
 import com.base.auth.model.User;
 import com.base.auth.model.criteria.UserCriteria;
 import com.base.auth.repository.*;
+import com.base.auth.service.MFAService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,6 +60,9 @@ public class UserController {
 
     @Autowired
     private ServiceRepository serviceRepository;
+
+    @Autowired
+    private MFAService mfaService;
 
     @PostMapping(value = "/signup", produces= MediaType.APPLICATION_JSON_VALUE)
     public ApiMessageDto<String> create(@Valid @RequestBody SignUpUserForm signUpUserForm, BindingResult bindingResult)
@@ -102,18 +110,28 @@ public class UserController {
     public ApiMessageDto<String> login(@Valid @RequestBody LoginForm loginForm, BindingResult bindingResult)
     {
         ApiMessageDto<String> apiMessageDto = new ApiMessageDto<>();
-
         Account account = accountRepository.findAccountByPhone(loginForm.getPhone());
         if (account==null||!passwordEncoder.matches((loginForm.getPassword()),account.getPassword()))
         {
-            apiMessageDto.setMessage("phone number or password is not correct ");
-            apiMessageDto.setCode(ErrorCode.USER_ERROR_LOGIN_FAILED);
-            apiMessageDto.setResult(false);
-            return apiMessageDto;
+            throw new BadRequestException("phone number or password is not correct");
         }
-        apiMessageDto.setMessage("Login Success");
+        User user = userRepository.findByAccountId(account.getId()).orElseThrow(()
+            -> new NotFoundException("User not found"));
+        if (!Boolean.TRUE.equals(user.getMfaEnabled())){
+            String qrCodeUrl = null;
+            if (StringUtils.isBlank(user.getMfaSecretKey())){
+                String secretKey = mfaService.generateSecretKeyForUser();
+                qrCodeUrl = mfaService.generateQrCodeUrl(secretKey, loginForm.getPhone());
+                user.setMfaSecretKey(secretKey);
+            }
+            user.setMfaEnabled(false);
+            userRepository.save(user);
+            apiMessageDto.setData(qrCodeUrl);
+        }
+        apiMessageDto.setMessage("Login success, please verify OTP");
         return apiMessageDto;
     }
+
     @GetMapping(value = "/get/{id}", produces= MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasRole('US_V')")
     public ApiMessageDto<UserDto> getUser(@PathVariable("id") Long id)
@@ -237,6 +255,38 @@ public class UserController {
         accountMapper.fromUpdateUserFormToEntity(updateUserForm,account);
         accountRepository.save(account);
         apiMessageDto.setMessage("update success");
+        return apiMessageDto;
+    }
+
+    @PutMapping(value = "/verify-otp-login", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiMessageDto<String> verifyOtpLogin(@RequestBody @Valid VerifyOtpLoginForm verifyOtpLoginForm, BindingResult bindingResult){
+        ApiMessageDto<String> apiMessageDto = new ApiMessageDto<>();
+        Account account = accountRepository.findAccountByPhone(verifyOtpLoginForm.getPhone());
+        if (account == null){
+            throw new NotFoundException("Account not found");
+        }
+        User user = userRepository.findByAccountId(account.getId()).orElseThrow(()
+        -> new NotFoundException("User not found"));
+        boolean isVerifyOtp = mfaService.verifyOtp(user.getMfaSecretKey(), verifyOtpLoginForm.getOtp());
+        if (!isVerifyOtp){
+            throw new BadRequestException("Invalid OTP");
+        }
+        user.setMfaEnabled(UserBaseConstant.MFA_ENABLE);
+        userRepository.save(user);
+        apiMessageDto.setMessage("Verify OTP success");
+        return apiMessageDto;
+    }
+
+    @PutMapping(value = "/restart-qrcode", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasRole('US_RT')")
+    public ApiMessageDto<String> restartQRCode(@RequestBody UserIdForm userIdForm, BindingResult bindingResult){
+        ApiMessageDto<String> apiMessageDto = new ApiMessageDto<>();
+        User user = userRepository.findById(userIdForm.getId()).orElseThrow(()
+        -> new NotFoundException("User not found"));
+        user.setMfaSecretKey(null);
+        user.setMfaEnabled(false);
+        userRepository.save(user);
+        apiMessageDto.setMessage("Restart QR code success");
         return apiMessageDto;
     }
 }
